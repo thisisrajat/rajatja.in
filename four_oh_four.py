f9df3935eb2ceda934a5a6e5f1ca31a6aaff4a5b
Rajat Jain <rajat.rj10@gmail.com>
2015-03-05 23:14:59 +0530

    friends 404 :)

---
+++ four_oh_four.py
@@ -0,0 +1,50 @@
+error_message = [
+  {
+    'message' : '15 Yemen Road, Yemen', 
+    'pre' : 'You\'ve reached', 
+    'media' : '../static/img/404/yemen.png',
+    'return_message' : 'Better get back'
+  },
+  {
+    'message' : 'Could you be in any more wrong place?',
+    'pre' : 'Chandler judges you',
+    'media' : '../static/img/404/judges.png',
+    'return_message' : 'Go back to where you belong, freak.'
+  },
+  {
+    'message' : 'We are on a break',
+    'pre' : 'Can\'t process your request because ',
+    'media': '../static/img/404/break.jpg',
+    'return_message': 'Go back.'
+  },
+  {
+    'message' : 'Welcome to the real world, it sucks. You\'re gonna love it',
+    'pre' : 'Made a typo huh?', 
+    'media' : '../static/img/404/monica.png',
+    'return_message': 'Go back :)'
+  },
+  {
+    'message' : 'Oh... My... Gawd...',
+    'pre' : 'You\'re doomed', 
+    'media' : '../static/img/404/ohmygod.png',
+    'return_message' : 'Quick, click me'
+  },
+  {
+    'message' : 'It\'s P as in "phoebe," H as in "hoebe," O as in "oebe," E as in "ebe," B as in "bebe," and E as in "ello there, mate!"',
+    'pre' : 'You need to spell like her',
+    'media' : '../static/img/404/pheobe.png',
+    'return_message' : 'Leave the weird land'
+  },
+  {
+    'message' : 'If I don\'t input those numbers... it doesn\'t make much of a difference.',
+    'pre' : 'Let me crunch the numbers... or not', 
+    'media': '../static/img/404/numbers.png',
+    'return_message' : 'Go back to the fellow processors'
+  },
+  {
+    'message' : 'Phoebe: Je m\'appelle Claude; Joey: Jet aplee blooo;',
+    'pre': 'Look Joey\'s just as good as you are in spelling out words',
+    'media' : '../static/img/404/french.png',
+    'return_message': 'Le felmno Poo?'
+  }
+]
