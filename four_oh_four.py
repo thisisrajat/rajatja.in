8e54c53b66c7fb377f632924743b9cc4ff26fb07
Rajat Jain <rajat.rj10@gmail.com>
2015-04-15 00:04:46 +0530

    exclamation points

--- four_oh_four.py
+++ four_oh_four.py
@@ -3,19 +3,19 @@ error_message = [
     'message' : '15 Yemen Road, Yemen', 
     'pre' : 'You\'ve reached', 
     'media' : '../static/img/404/yemen.png',
-    'return_message' : 'Better get back'
+    'return_message' : 'Better get back!'
   },
   {
     'message' : 'Could you be in any more wrong place?',
     'pre' : 'Chandler judges you',
     'media' : '../static/img/404/judges.png',
-    'return_message' : 'Go back to where you belong, freak.'
+    'return_message' : 'Go back to where you belong, freak!'
   },
   {
     'message' : 'We are on a break',
     'pre' : 'Can\'t process your request because ',
     'media': '../static/img/404/break.jpg',
-    'return_message': 'Go back before the dinosaurs.'
+    'return_message': 'Go back before the dinosaurs!'
   },
   {
     'message' : 'Welcome to the real world, it sucks. You\'re gonna love it',
@@ -27,7 +27,7 @@ error_message = [
     'message' : 'Oh... My... God...',
     'pre' : 'You\'re doomed', 
     'media' : '../static/img/404/ohmygod.png',
-    'return_message' : 'Quick, click me.'
+    'return_message' : 'Quick, click me!'
   },
   {
     'message' : 'It\'s P as in "phoebe," H as in "hoebe," O as in "oebe," E as in "ebe," B as in "bebe," and E as in "ello there, mate!"',
@@ -39,12 +39,12 @@ error_message = [
     'message' : 'If I don\'t input those numbers... it doesn\'t make much of a difference.',
     'pre' : 'Let me crunch the numbers... or not', 
     'media': '../static/img/404/numbers.png',
-    'return_message' : 'Go back to the fellow processors'
+    'return_message' : 'Go back to the fellow processors!'
   },
   {
     'message' : 'Phoebe: Je m\'appelle Claude; Joey: Jet aplee blooo;',
     'pre': 'Look Joey\'s just as good as you, in spelling out words',
     'media' : '../static/img/404/french.png',
-    'return_message': 'Le felmno Poo?'
+    'return_message': 'Le felmno Poo!'
   }
 ]
