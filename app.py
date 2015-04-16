b12fd6edfe54a133859f5f4ae4546367ea5e303b
Rajat Jain <rajat.rj10@gmail.com>
2015-04-16 16:37:33 +0530

    import exchange

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, make_response, redirect
 from random import randint, shuffle
-import os
 from four_oh_four import error_message
+import os
 
 app = Flask(__name__)
 
