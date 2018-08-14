9864652a567a7ce07429b876f5a2ab19d591822b
Rajat <rajat.rj10@gmail.com>
2018-08-14 21:27:06 +0530

    Add gh deploy script and typo fix

---
+++ gh-pages-deploy.sh
@@ -0,0 +1,8 @@
+yarn install
+yarn build
+git checkout gh-pages
+rsync -a -v build/ ./
+rm -rf build
+echo 'Enter commit message'
+read commitmsg
+git commit -am commitmsg
--- src/connect/index.js
+++ src/connect/index.js
@@ -6,7 +6,7 @@ function ConnectPage() {
   return (
     <div className="app-wrapper dimmer connect-page">
       <div className="copy-text">
-        Drop me an email on <a href="mailto:rajat@rajatja.in">rajat@rajat.in</a>. I'll try to get back to you as soon as possible. Alternatively, you can also reach me on Twitter <a href="https://www.twitter.com/thisisrajat/">@thisisrajat</a>.
+        Drop an email on <a href="mailto:rajat@rajatja.in">rajat@rajatja.in</a>. Alternatively, you can also reach me on Twitter <a href="https://www.twitter.com/thisisrajat/">@thisisrajat</a>.
       </div>
     </div>
   );
