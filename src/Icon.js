bd56acc6d75c302452519161c686e8c57cee68c8
Rajat <rajat.rj10@gmail.com>
2018-08-14 20:17:03 +0530

    Init with nothing but CNAME

--- src/Icon.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Icon(props) {
-  return <img height="32" width="32" {...props} />;
-}
-
-export default Icon;
--- src/utils/getRandomNumber.js
+++
@@ -1,3 +0,0 @@
-export function getRandomNumber(startRange, endRange) {
-  return Math.floor(Math.random() * (endRange - startRange + 1)) + startRange;
-}
